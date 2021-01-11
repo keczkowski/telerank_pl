e0e4ab0c5e90c8462c3490a5a0edb51d04a34dbe
Krzysztof Keczkowski <krzysztof.keczkowski@gmail.com>
2021-01-11 11:49:35 +0100

    layout i logowanie

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -34,4 +34,14 @@ module ApplicationHelper
       end
     end
 
+    def flash_messages(opts = {})
+      flash.each do |msg_type, message|
+        concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)}", role: "alert") do
+                concat content_tag(:button, 'x', class: "close", data: { dismiss: 'alert' })
+                concat message
+              end)
+      end
+      nil
+    end
+
 end
