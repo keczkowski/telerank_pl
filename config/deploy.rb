9274904b8e82d91585b383918235131ac2390678
Krzysztof Keczkowski <krzysztof.keczkowski@gmail.com>
2021-02-19 16:21:21 +0100

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.15.0"
 
-set :application, "myapp"
+set :application, "telerank_pl"
 set :repo_url, "git@github.com:keczkowski/telerank_pl.git"
 
 
